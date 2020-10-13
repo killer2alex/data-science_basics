051ef1ef29abfc1fa9e6dd9be4be19b49a20d3c1
Alex H <alexander.hoerig@gmail.com>
2020-10-13 15:23:40 +0200

    midline commit

---
+++ N1_T1_import_help_1.py
@@ -0,0 +1,2 @@
+# Wir benötigen 3 Pakete, welche auch bereits im ersten Notebook besprochen wurden.
+# Gucke am besten dort nochmal nach ;-)
---
+++ N2_T1_A1.py
@@ -0,0 +1,7 @@
+"""
+Diese Aufgaben stellen iuatrenuirtaen urtiane rtuinaeru nia
+uinartenu itranertuina ertunia ertnuia
+e uintaen urtiane ui
+"""
+
+### N1A1
