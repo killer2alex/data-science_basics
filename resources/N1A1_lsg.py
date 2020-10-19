33d50cc557580cf6d498c8b2ddeff51c9efff85d
Alex H <alexander.hoerig@gmail.com>
2020-10-19 18:20:28 +0200

    sabrina improvements

--- resources/N1A1_lsg.py
+++ resources/N1A1_lsg.py
@@ -6,13 +6,13 @@
 s_float = pd.Series(np.random.random(10))
 
 ## Series mit 10 Elementen, aus der Menge M = {1,2,3,4,5,6}
-s_int = pd.Series([1,5,6,3,2,1,4,5,2,1])
+s_int = pd.Series([1, 5, 6, 3, 2, 1, 4, 5, 2, 1])
 
 ## Series mit 10 unterschiedlichen Vornamen (alles klein geschrieben)
-s_string = pd.Series(["alex", "paul", "paula","erika", "jan","karl", "vlad", "franziska", "sabrina", "florian"])
+s_string = pd.Series(["alex", "paul", "paula", "erika", "jan", "karl", "vlad", "franzi", "bine", "flo"])
 
 ## Series `s_mix` mit 10 Einträgen, in der Zeichenketten, ganze Zahlen und Gleitkommazahlen vorkommen 
-s_mix = pd.Series([1,"zwei", 3.0, 4, "fünf", 6.111, "zieben", 8, 9.9999, 10])
+s_mix = pd.Series([1, "zwei", 3.0, 4, "fünf", 6.111, "sieben", 8, 9.9999, 10])
 
 ## Ausgabe der Series
 print(s_float, s_int, s_string, s_mix, sep="\n\n")
